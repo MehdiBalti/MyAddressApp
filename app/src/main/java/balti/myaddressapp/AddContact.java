5b275ff2951ea7b5210bc545b5a815e31a18936f
MehdiBalti <himehdi2012@gmail.com>
2017-03-17 22:40:16 -0700

    mehdi

--- app/src/main/java/balti/myaddressapp/AddContact.java
+++ app/src/main/java/balti/myaddressapp/AddContact.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 import android.widget.EditText;
 
 public class AddContact extends AppCompatActivity {
-EditText  edtnumber,edtname,edtemail;
+EditText  edtnumber,edtname,edtemail,edt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
